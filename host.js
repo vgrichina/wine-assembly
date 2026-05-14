c9e712b0fa4dbeb4642dabf0175e9e3f86ad8898
Vladimir Grichina <vgrichina@gmail.com>
2026-05-14 03:04:51 -0700

    Bump web cache for Pinball menu fix

--- host.js
+++ host.js
@@ -2,7 +2,7 @@
 // Win98Renderer is loaded from lib/renderer.js (included via <script> in index.html)
 
 class WineAssembly {
-  static SOURCE_VERSION = '129';
+  static SOURCE_VERSION = '130';
 
   constructor() {
     this.instance = null;
