1af0df782404d7913235ba7cbee123f0a4279fce
Vladimir Grichina <vgrichina@gmail.com>
2026-06-18 21:41:43 -0700

    Refresh web harness cache version

--- host.js
+++ host.js
@@ -2,7 +2,7 @@
 // Win98Renderer is loaded from lib/renderer.js (included via <script> in index.html)
 
 class WineAssembly {
-  static SOURCE_VERSION = '159';
+  static SOURCE_VERSION = '160';
 
   constructor() {
     this.instance = null;
