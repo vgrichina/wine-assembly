73f007bcc5669a4a71dbae7642aa3866c7932b16
Vladimir Grichina <vgrichina@gmail.com>
2026-07-16 19:22:31 -0700

    Make Winamp EQ controls easier to press

--- host.js
+++ host.js
@@ -2,7 +2,7 @@
 // Win98Renderer is loaded from lib/renderer.js (included via <script> in index.html)
 
 class WineAssembly {
-  static SOURCE_VERSION = '167';
+  static SOURCE_VERSION = '168';
 
   constructor() {
     this.instance = null;
