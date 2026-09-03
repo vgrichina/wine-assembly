6c998b0beb87446090f62ef57c3fdce12528946c
Vladimir Grichina <vgrichina@gmail.com>
2026-09-02 21:02:08 -0700

    Tier frozen CLI recording test

--- test/run-all.sh
+++ test/run-all.sh
@@ -642,6 +642,7 @@ E2E=(
   test/test-wordpad-web.js
   test/test-wordpad-thread-startup.js
   test/test-cli-worker-threads.js
+  test/test-cli-frozen-recording.js
   test/test-wordpad-copy-clipboard.js
   test/test-wordpad-paste-refcount.js
   test/test-wordpad-font-combo.js
